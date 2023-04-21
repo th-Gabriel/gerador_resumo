59ac460a2d7b9f5123650f069dc2052ac2310650
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2023-04-21 12:44:25 -0300

    Classe GeradorResumo que contém as funcionalidades foi criada

---
+++ src/GeradorResumo.py
@@ -0,0 +1,30 @@
+import wikipedia
+from fpdf import FPDF
+
+
+class GeradorResumo:
+    def __init__(self):
+        pass
+
+
+    def pesquisar_wikipedia(self):
+        self.tema = input('DIGITE O TEMA:').upper()
+
+        wikipedia.set_lang('pt')
+        try:
+            pagina = wikipedia.page(self.tema)
+            self.resumo = pagina.summary
+        except wikipedia.exceptions.DisambiguationError as err:
+            print('por favor, seja mais específico')
+        except wikipedia.exceptions.PageError as err:
+            print('página não encontrada')
+
+
+    def gerar_pdf(self):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Arial', size=12)
+        pdf.cell(0, 10, txt=self.tema, align='C')
+        pdf.ln()
+        pdf.multi_cell(180, 10, txt=self.resumo, align='J', )
+        pdf.output(f'resumo_{self.tema}.pdf')
